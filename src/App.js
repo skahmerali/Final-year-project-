60b002aa1c9d3d54dd7d2e394015fcfefe2eecae
SheikhAhmer <sheikhahmerali75@gmail.com>
2022-09-14 11:55:03 +0500

    Final Year Project

---
+++ src/App.js
@@ -0,0 +1,29 @@
+// import { Switch } from '@mui/material';
+// import { Routes, Route } from 'react-router-dom';
+
+import Signin from "./LoginContainer/Signin";
+
+// import Home from './components/Home';
+// import About from './components/About';
+// import Blogs from './components/Blogs';
+
+
+function App() {
+  
+  return (
+    <>
+ <Signin />
+     
+    {/* <Routes>
+      <Route exact path='/' element={<Home/>}/>
+      <Route exact path='/about' element={<About/>}/>
+      <Route exact path='/blogs' element={<Blogs/>}/>
+    </Routes> */}
+
+ 
+      <h1>hello</h1>
+    </>
+  )
+}
+
+export default App;
